fbc88dd08b03c89cb2c7e78f5a7d634c3546e5ac
darkjoy2k2-crypto <darkjoy2k2@googlemail.com>
2025-12-11 08:12:06 +0100

    what a mess

--- out/res/title.d
+++
@@ -1,5 +0,0 @@
-out/res/title.o: res/title.res \
-C:/Users/peter/Documents/_Genesis/Game2/res/ball.png \
-C:/Users/peter/Documents/_Genesis/Game2/res/level_0.png \
-C:/Users/peter/Documents/_Genesis/Game2/res/layer_bg.png \
-C:/Users/peter/Documents/_Genesis/Game2/res/message_sign.png
